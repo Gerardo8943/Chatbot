5a1c09026a46ac4d27d17c8702e3da74110f20f5
Gerardo <torresanibal388@gmail.com>
2025-12-06 21:56:35 -0400

    Primeras bases del proyecto usando react y el backend con python

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import RobotLogo from "./components/RobotLogo";
+
+function App() {
+  return (
+    <div>
+      <RobotLogo />
+      <h2>Tu proyecto React está funcionando 🎉</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/RobotLogo.tsx
@@ -0,0 +1,7 @@
+export default function RobotLogo(){
+    return(
+         <div style={{ textAlign: "center", marginTop: "30px" }}>
+      <h1>🤖 Hola, soy un robot!</h1>
+    </div>
+    );
+}
